1a8a8f6d9304e531beb5134e4b5599345a72a462
oded.sadeh@global-e.com <oded.sadeh@global-e.com>
2026-04-29 13:57:01 +0000

    order_base_table

---
+++ models/stg/stg_fact_orders_basetable.sql
@@ -0,0 +1,71 @@
+SELECT
+    O.OrderId,
+    O.OrderRecId,
+    O.DateCreated,
+    O.DwhDateLastUpdated,
+    O.MerchantId,
+    O.ShipCountryId,
+    O.HubId,
+    O.SOURCECOUNTRYID, 
+    O.OrderStatusId,
+    O.CurrencyId,
+    O.ShippingMethodId,
+    CASE WHEN PT.PaymentOptionTypeId = 1 THEN -1 ELSE O.PaymentMethodId END AS PaymentMethodId,
+    O.EstimatedShippingWeight,
+    CASE WHEN O.OriginalOrderId IS NOT NULL THEN 'Yes' ELSE 'No' END AS IsReplacementOrder,
+    CASE WHEN TotalEndCustomerShippingPrice = 0 THEN 'Yes' ELSE 'No' END AS IsFreeShipping,
+    CASE WHEN O.OrderStatusId NOT IN (3 /*Canceled by Global-E*/, 9	/*Canceled by Merchant*/, 20	/*Canceled by customer*/, 2	/*Payment Declined*/, 5	/*Pending Payment*/) AND YEAR(ODD.DateReceivedByGlobalE) > 1900 THEN 'Yes' ELSE 'No' END AS IsApprovedOrder,
+    O.ActivePaymentTransactionId,
+    O.RiskStatusId,
+    O.OrderedOriginalTotalShippingPrice,
+    O.OriginalTotalShippingPrice,
+    O.TransactionTotalPrice,
+    OS.OrderStatusDisplayName,
+    PG.PaymentGatewayName,
+    CASE WHEN OED.IsMobile = 1 THEN 'Mobile'
+     WHEN OED.IsMobile = 0 THEN 'Desktop' 
+     ELSE 'Unknown' END AS PlatformType,
+    OED.IsTaxCollectedByGlobalE AS IS_TAX_COLLECTED_BY_GLOBALE,
+    OED.ISB2B,
+    CASE WHEN OED.ORDERPROCESSINGTYPEID = 2 THEN 1 ELSE 0 END AS IS_3B2C,
+    OED.CODFeeCustomerCurrency,
+    OCD.HashShipEmail,
+    S.StateName AS ShipStateName,
+    ST.Name as storeName, /*Original storeName */
+    OCD2.ShipCity,
+    OCD2.ShipZip,
+    COALESCE(ODD.DateReceivedByGlobalE,TO_DATE('1900-01-01', 'YYYY-MM-DD')) AS OrderDate,
+    RD.ReconciliationDate AS ReconciliationDate,
+    FirstSM.ShippingMethodId AS CustomerSelectedFirstShippingMethod,
+    O.ORIGINALCURRENCYID,
+    M.MERCHANTCURRENCYID
+
+FROM shared_prod_datalake.GLOBALE_DBO.Orders O
+INNER JOIN {{ ref('orders_pop') }}  P ON P.OrderId = O.OrderId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.OrderStatuses OS ON O.OrderStatusId = OS.OrderStatusId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.PaymentTransactions PT ON O.ActivePaymentTransactionId = PT.PaymentTransactionId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.PaymentGateways PG ON PT.PaymentGatewayId = PG.PaymentGatewayId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.ORDEREXTRADETAILS OED ON O.OrderId = OED.OrderId
+INNER JOIN shared_prod_datalake.GLOBALE_SECURED.ORDERCUSTOMERDETAILS OCD ON O.OrderRecId = OCD.OrderRecId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.Countries C ON O.ShipCountryId = C.CountryId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.Merchants M ON O.MerchantId = M.MerchantId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.Countries MC ON M.CountryId = MC.CountryId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.States S ON O.ShipStateId = S.StateId
+INNER JOIN shared_prod_datalake.GLOBALE_DBO.Stores ST ON O.StoreId = ST.StoreId
+LEFT JOIN  shared_prod_datalake.GLOBALE_DBO.OrderCustomerDetails OCD2 ON OCD2.OrderRecId = O.OrderRecId
+LEFT JOIN  shared_prod_datalake.GLOBALE_DBO.ORDERDELIVERYDETAILS ODD ON ODD.OrderId = O.OrderId AND ODD.ParcelId IS NULL
+LEFT JOIN {{ ref('stg_lkp_reconciliationdate') }} RD ON RD.OrderId = O.OrderId
+LEFT JOIN LATERAL (
+    SELECT Case When Count(par.ParcelId) = 0 then 1 else Count(par.ParcelId) end as ParcelsCount
+    FROM shared_prod_datalake.GLOBALE_DBO.Parcels par 
+    WHERE Par.OrderId = P.OrderId
+) Parcels
+
+LEFT JOIN LATERAL(
+
+        SELECT 
+        MIN_BY(VO.ShippingMethodId,VO.DateLastUpdated) as ShippingMethodId
+        FROM datalake.GLOBALE_DBO.OrderShippingMethodsHistory VO
+        WHERE VO.OrderId=P.OrderId 
+
+) FirstSM
